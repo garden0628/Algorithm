eeabcb72378869929bdca1506d57a0a6bd86cdec
JeongWon Kim <76953545+garden0628@users.noreply.github.com>
2023-06-18 23:32:14 +0900

    Create 백준9663번-N-Queen.py

---
+++ 백준9663번-N-Queen.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+answer = 0
+row = [0]*n
+
+def is_check(x):
+    for i in range(x):
+        if row[x]==row[i] or abs(row[x]-row[i])==abs(x-i):
+            return False
+    return True
+
+def n_queen(x):
+    global answer
+    if x == n:
+        answer += 1
+        return
+    else:
+        for i in range(n):
+            row[x] = i
+            if is_check(x):
+                n_queen(x+1)
+
+n_queen(0)
+print(answer)
