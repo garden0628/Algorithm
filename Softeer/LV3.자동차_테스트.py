8713976b276001cce0394727afdc18bf1d1ca777
JeongWon Kim <76953545+garden0628@users.noreply.github.com>
2023-09-28 17:07:07 +0900

    Create LV3.자동차_테스트.py

---
+++ Softeer/LV3.자동차_테스트.py
@@ -0,0 +1,20 @@
+import sys
+
+n, q = map(int, sys.stdin.readline().split())
+cost = list(map(int, sys.stdin.readline().split()))
+cost.sort()
+
+value = [0] * len(cost)
+idx = dict()
+for i in range(len(cost)):
+    value[i] = i * (len(cost)-i-1)
+    idx[cost[i]] = i
+
+cost = set(cost)
+for _ in range(q):
+    num = int(sys.stdin.readline())
+    if num not in cost:
+        print(0)
+    else:
+        # idx = cost.index(num)
+        print(value[idx[num]])
